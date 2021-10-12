ad1066215ca1a1b8f1c737d654ff24f2a66361b8
Shounak Mulay <shounakmulay@gmail.com>
2021-10-12 12:10:21 +0530

    Remove unused code.

--- app/src/main/java/com/example/kotlinflowtest/repository/MainRepository.kt
+++ app/src/main/java/com/example/kotlinflowtest/repository/MainRepository.kt
@@ -1,10 +1,7 @@
 package com.example.kotlinflowtest.repository
 
-import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
-    val counterFlow: Flow<Int>
-    val squareFlow: Flow<Int>
 
     suspend fun repositorySuspendingFunction()
 }
--- app/src/main/java/com/example/kotlinflowtest/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/kotlinflowtest/repository/MainRepositoryImpl.kt
@@ -1,18 +1,7 @@
 package com.example.kotlinflowtest.repository
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 
 class MainRepositoryImpl: MainRepository {
-    override val counterFlow = flow {
-        emit(1)
-        delay(1000)
-    }
-
-    override val squareFlow = counterFlow.map {
-        it * it
-    }
 
     override suspend fun repositorySuspendingFunction() {
         return
